d7b9e2665f7b43dcb733b2151f7f5cc3abf9fe4e
Stefan Siegl <stesie@brokenpipe.de>
2019-01-11 14:38:01 +0100

    reformat

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
 import './App.css';
 import 'semantic-ui-css/semantic.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        <div><Button primary={true}>Click Here</Button></div>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div>
+                        <Button primary={true}>Click Here</Button>
+                    </div>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
