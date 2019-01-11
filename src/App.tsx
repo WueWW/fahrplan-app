1b3689b2b8480a41cbac9b1a40aed54f1dcde650
Stefan Siegl <stesie@brokenpipe.de>
2019-01-11 14:35:20 +0100

    add semantic-ui-react + css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
 import './App.css';
+import 'semantic-ui-css/semantic.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <div><Button primary={true}>Click Here</Button></div>
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
