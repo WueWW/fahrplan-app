b45ebb89c20f437f5067c8c10e9e3d21262c9eb6
Stefan Siegl <stesie@brokenpipe.de>
2019-01-15 23:03:34 +0100

    display all session information

--- src/model/Session.ts
+++ src/model/Session.ts
@@ -1,4 +1,11 @@
 export interface Session {
     key: string;
+    start: string;
+    end: string;
+    host: string;
     title: string;
+    location: string;
+    short_description: string;
+    long_description: string;
+    link: string;
 }
