49ecbc4a26bd7c79ad2b66566c8e6987252d6c98
Stefan Siegl <stesie@brokenpipe.de>
2019-01-24 19:15:12 +0100

    reference sw.js via relative uri

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ ReactDOM.render(
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('/sw.js')
+            .register('sw.js')
             .then(registration => {
                 console.log('SW registered: ', registration);
             })
