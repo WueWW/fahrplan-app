5820660e49d149d3dabf884ef18326f3bc148269
Stefan Siegl <stesie@brokenpipe.de>
2019-01-25 10:41:34 +0100

    format date, closes #19

--- src/component/SessionDatePicker.tsx
+++ src/component/SessionDatePicker.tsx
@@ -8,8 +8,12 @@ export interface Props {
     onDateSelected: (selectedDate: string) => void;
 }
 
+function formatDate(d: string): string {
+    return new Date(d).toLocaleDateString('de-de', { weekday: 'long', year: undefined, month: 'long', day: 'numeric' });
+}
+
 function buildOptions(options: string[]) {
-    return options.map(option => ({ text: option, value: option }));
+    return options.map(option => ({ text: formatDate(option), value: option }));
 }
 
 const SessionDatePicker: React.FunctionComponent<Props> = props => (
