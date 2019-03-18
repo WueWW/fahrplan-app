8887be19840efcf6ac660531710ac4843d714fa5
Stefan Siegl <stesie@brokenpipe.de>
2019-03-18 21:32:03 +0100

    link to apple maps on iphone et al

--- src/component/SessionTable/LocationBlock.tsx
+++ src/component/SessionTable/LocationBlock.tsx
@@ -9,7 +9,27 @@ export interface Props {
 
 const distanceDivStyle = { marginLeft: '1.5rem' };
 
-const isMobileDevice = (): boolean => /iPad|iPhone|iPod|android/i.test(navigator.userAgent);
+const isAppleDevice = (): boolean => /iPad|iPhone|iPod/i.test(navigator.userAgent);
+
+const isAndroidDevice = (): boolean => /android/i.test(navigator.userAgent);
+
+const isMobileDevice = (): boolean => isAppleDevice() || isAndroidDevice();
+
+const geoLink = (location: Location): string | undefined => {
+    if (location.lat === undefined || location.lng === undefined) {
+        return undefined;
+    }
+
+    if (isAppleDevice()) {
+        return `http://maps.apple.com/?ll=${location.lat},${location.lng}`;
+    }
+
+    if (isAndroidDevice()) {
+        return `geo:${location.lat},${location.lng}`;
+    }
+
+    return `https://www.openstreetmap.org/?mlat=${location.lat}&mlon=${location.lng}&zoom=16`;
+};
 
 const LocationBlock: FunctionComponent<Props> = ({ location }) => {
     if (!location || !location.name) {
@@ -21,19 +41,12 @@ const LocationBlock: FunctionComponent<Props> = ({ location }) => {
         .map((x: string) => x.trim())
         .filter((x: string) => x);
 
-    const geoLink =
-        location.lat !== undefined &&
-        location.lng !== undefined &&
-        (isMobileDevice()
-            ? `geo:${location.lat},${location.lng}`
-            : `https://www.openstreetmap.org/?mlat=${location.lat}&mlon=${location.lng}&zoom=16`);
-
     return (
         <Card.Content extra>
             <Card.Description>
                 <Icon className="left floated" name="globe" />
                 <div style={distanceDivStyle}>
-                    <a href={geoLink || undefined} target={isMobileDevice() ? undefined : '_blank'}>
+                    <a href={geoLink(location)} target={isMobileDevice() ? undefined : '_blank'}>
                         {parts[0]}
                     </a>
                 </div>
