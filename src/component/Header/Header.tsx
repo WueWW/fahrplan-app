22c01c54f4f4387d4a4a47dcb698f1077832a408
Stefan Siegl <stesie@brokenpipe.de>
2021-09-17 11:42:10 +0200

    use 2021 logo

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -2,6 +2,6 @@ import './style.less';
 
 import React from 'react';
 
-const Header: React.FunctionComponent = () => <img id="logo" src="assets/wueww-logo-2020.svg" />;
+const Header: React.FunctionComponent = () => <img id="logo" src="assets/wueww-logo-2021.svg" />;
 
 export default Header;
