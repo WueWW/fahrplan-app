81f278e7d54eb9b0c24c26f693bafbedf1fe5374
Stefan Siegl <stesie@brokenpipe.de>
2021-09-17 11:36:43 +0200

    Revert "strip created by line"
    This reverts commit 3bf73b80b45be260aeefb8b0ab2fa5cb35315506.

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -4,6 +4,7 @@ import { Icon } from 'semantic-ui-react';
 
 const Footer: FunctionComponent = () => (
     <footer>
+        Made with <Icon name="heart outline" size="small" color="red" /> by Rolf |{' '}
         <Link to="/impressum">Impressum</Link>
     </footer>
 );
