ed40eec5ca8b68c88fb5ef5dc219ba67744961fc
Stefan Siegl <stesie@brokenpipe.de>
2022-08-26 21:37:36 +0200

    use 2022 logo + pink color

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -2,6 +2,6 @@ import './style.less';
 
 import React from 'react';
 
-const Header: React.FunctionComponent = () => <img id="logo" src="assets/wueww-logo-2021.svg" />;
+const Header: React.FunctionComponent = () => <img id="logo" src="assets/wueww-logo-2022.svg" />;
 
 export default Header;
