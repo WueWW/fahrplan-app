d5fa4cd6d5b55f3bab875f020aff6c912a8760a6
Stefan Siegl <stesie@brokenpipe.de>
2022-08-27 12:32:20 +0200

    optionally show long description

--- src/component/SessionTable/Session/SessionDescription.tsx
+++ src/component/SessionTable/Session/SessionDescription.tsx
@@ -1,15 +1,49 @@
 import { Card } from 'semantic-ui-react';
 import { Session } from '../../../model/Session';
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 interface SessionDescriptionProps {
     description: Session['description'];
 }
 
-const SessionDescription = (props: SessionDescriptionProps) => (
-    <Card.Content extra>
-        <Card.Description>{props.description?.short}</Card.Description>
-    </Card.Content>
-);
+const SessionDescription = (props: SessionDescriptionProps) => {
+    const [showMore, setShowMore] = useState(false);
+
+    return (
+        <Card.Content>
+            <Card.Description>
+                <p>
+                    {props.description?.short}
+                    {props.description?.long && !showMore ? (
+                        <>
+                            {' '}
+                            <a
+                                href="#"
+                                onClick={(ev) => {
+                                    ev.preventDefault();
+                                    setShowMore(true);
+                                }}
+                            >
+                                mehr
+                            </a>
+                        </>
+                    ) : null}
+                </p>
+                {props.description?.long &&
+                    showMore &&
+                    props.description?.long.split(/\r?\n\r?\n/).map((text, i) => (
+                        <p key={i}>
+                            {text.split(/\r?\n/).map((line, i) => (
+                                <Fragment key={i}>
+                                    {line}
+                                    <br />
+                                </Fragment>
+                            ))}
+                        </p>
+                    ))}
+            </Card.Description>
+        </Card.Content>
+    );
+};
 
 export default SessionDescription;
